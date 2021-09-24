b062d8959fab108f96a935fac248af69416989a0
korbandras <korbandras@gmail.com>
2021-09-24 16:26:27 +0200

    2. óra szorgalmi

---
+++ numtodate.py
@@ -0,0 +1,13 @@
+def time():
+    num = int(input('Insert a number (>0): '))
+    yr = num // (60*60*24*365)
+    num = num % (365*24*3600)
+    day2 = num // (60*60*24)
+    num = num % (24*3600)
+    hr = (num // 3600)
+    num %= 3600
+    minute = num // 60
+    num %= 60
+    sec = num
+    print('%d év, %d nap, %d óra, %d perc, %d másodperc' %(yr, day2, hr, minute, sec))
+time()
